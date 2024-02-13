839bc919af049a54c0b6efdf64e3507d598a3452
max <max.assis@chatguru.com.br>
2024-02-13 20:05:07 -0300

    fazendo endpoint de criação de todo

--- routes.ts
+++ routes.ts
@@ -2,42 +2,66 @@ import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } f
 import { CreateUserController } from "./src/Controllers/UserController";
 import jwt from 'jsonwebtoken'
 import { prisma } from "./src/Services/prismaService";
+import { z } from 'zod'
+import { TodoController } from "./src/Controllers/TodosController";
 
 const userController = new CreateUserController()
+const todoController = new TodoController()
 
-type MyToken = {
-    id: string
-    email: string
-    iat: number
-    exp: number
-  }
+const MyToken = z.object({
+    id: z.string(),
+    email: z.string().email(),
+    iat: z.number(),
+    exp: z.number()
+})
 
+const TodoBodyDTO = z.object({
+    id: z.string().uuid(),
+    content: z.string()
+})
+
+interface RequestWithUser extends FastifyRequest {
+    user? : {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+    }
+}
+
+const preHandler = {preHandler : async (request: RequestWithUser, reply: FastifyReply, done: () => void) => {
+    const token = request.headers.authorization?.split(' ')[1]
+    if(!token) reply.code(401).send({error: "token invalido"})
+    const decodedToken = MyToken.parse(jwt.verify(token!, process.env.JWT_SECRET as string))
+
+    try {            
+       const user = await prisma.user.findUnique({where: {id: decodedToken.id}}) 
+       request.user = user! 
+       done()
+    } catch(e: any) {
+        reply.code(401).send({error: "token invalido"})
+    }
+}}
+
+ 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
     
-   fastify.post('/signin', {preHandler : async (request, reply, done) => {
-        const token = request.headers.authorization?.split(' ')[1]
-        if(!token) reply.code(401).send({error: "token invalido"})
-
-        const decodedToken: any = jwt.verify(token!, process.env.JWT_SECRET as string) as MyToken
-
-        try {            
-           const user = await prisma.user.findUnique({where: {id: decodedToken.id}})
-           console.log(user)
-           done()
-        } catch(e: any) {
-            reply.code(401).send({error: "token invalido"})
-        }
-
-    }}, async (request: FastifyRequest, reply: FastifyReply) => {
+   fastify.post('/signin', preHandler , async (request: RequestWithUser, reply: FastifyReply) => {
         return userController.create(request, reply)
     })
 
-    fastify.post('/login', async (request: FastifyRequest, reply: FastifyReply) => {
+    fastify.post('/login', preHandler, async (request: RequestWithUser, reply: FastifyReply) => {
         return userController.login(request, reply)
     })
 
-    fastify.post('/create_todo', async (request: FastifyRequest, reply: FastifyReply) => {
-        return "create todo"
+    fastify.post('/create_todo', preHandler , async (request: RequestWithUser, reply: FastifyReply) => {
+        console.log(request.body.content)
+
+        return todoController.create(request.user?.id!, request.body.content )
     })
+
+    
 
 }
